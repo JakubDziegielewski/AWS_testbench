0ac5b47bab912a787e2b20e06b28cfe8b28c05a0
JakubDziegielewski <kubadziegiel1@gmail.com>
2022-12-18 21:01:40 +0100

    next tests added

--- lib/iam_test.py
+++ lib/iam_test.py
@@ -10,6 +10,21 @@ import csv
 aws = AWSAPI()
 
 
+def generate_and_save_credntial_report(report_file, aws_api, credential_report="credential_report.csv"):
+    try:
+        aws.execute(
+            ["iam", "generate-credential-report"])
+        output = aws_api.execute(["iam", "get-credential-report"])
+    except AWSCLIError as e:
+        with open(report_file, 'a') as rf:
+            rf.write(
+                f"An error ocured while generating new credential report: {e}\n")
+    else:
+        encoded_report = json.loads(output)["Content"]
+        with open("credential_report.csv", "w") as cr:
+            cr.write(base64.b64decode(encoded_report).decode("utf-8"))
+
+
 def no_root_access_key_exist(report_file, aws_api):
     try:
         output = aws_api.execute(["iam", "get-account-summary"])
@@ -33,7 +48,7 @@ def multifactor_auth_for_root(report_file, aws_api):
     try:
         output = aws_api.execute(["iam", "get-account-summary"])
     except AWSCLIError as e:
-        with open(report_file, 'a') as rf:
+        with open(report_file, "a") as rf:
             rf.write(
                 f"An error ocured while running test multifactor_auth_for_root: {e}\n")
     else:
@@ -44,40 +59,26 @@ def multifactor_auth_for_root(report_file, aws_api):
                 rf.write("mulfifactor authentication for root enabled: True\n")
         else:
             with open(report_file, 'a') as rf:
-                rf.write("mulfifactor authentication for root enabled: False\n")
+                rf.write("ALERT: mulfifactor authentication for root enabled: False\n")
 
 
 def when_root_was_last_used(report_file, aws_api):
-    try:
-        create_new_report = aws_api.execute(
-            ["iam", "generate-credential-report"])
-        print(create_new_report)
-        output = aws_api.execute(["iam", "get-credential-report"])
-
-    except AWSCLIError as e:
-        print(
-            f"An error ocured while running test when_root_was_last_used: {e}\n")
-
-    else:
-        encoded_report = json.loads(output)["Content"]
-        with open("credential_report.csv", "w") as cr:
-            cr.write(base64.b64decode(encoded_report).decode("utf-8"))
-        with open("credential_report.csv", "r") as cr:
-            csv_reader = csv.reader(cr, delimiter=",")
-            for row in csv_reader:
-                if row[0] == "<root_account>":
-                    password_last_used_date = datetime.strptime(
-                        row[4][:10] + " " + row[4][11:19], "%Y-%m-%d %H:%M:%S")
-                    current_date = datetime.now()
-                    difference = current_date - password_last_used_date
-                    if difference.days < 7:
-                        with open(report_file, 'a') as rf:
-                            rf.write(
-                                f"ALERT: root acccount was accessed in the last 7 days: Password was last used: {row[4]}\n")
-                    else:
-                        with open(report_file, 'a') as rf:
-                            rf.write(
-                                f"Root acccount was not accessed in the last 7 days: Password was last used: {row[4]}, Access key 1 was last used: {row[10]}, Access key 2 was last used: {row[15]}\n")
+    with open("credential_report.csv", "r") as cr:
+        csv_reader = csv.reader(cr, delimiter=",")
+        for row in csv_reader:
+            if row[0] == "<root_account>":
+                password_last_used_date = datetime.strptime(
+                    row[4][:10] + " " + row[4][11:19], "%Y-%m-%d %H:%M:%S")
+                current_date = datetime.now()
+                difference = current_date - password_last_used_date
+                if difference.days < 7:
+                    with open(report_file, 'a') as rf:
+                        rf.write(
+                            f"ALERT: root acccount was accessed in the last 7 days: Password was last used: {row[4]}\n")
+                else:
+                    with open(report_file, 'a') as rf:
+                        rf.write(
+                            f"Root acccount was not accessed in the last 7 days: Password was last used: {row[4]}, Access key 1 was last used: {row[10]}, Access key 2 was last used: {row[15]}\n")
 
 
 def password_longer_than_14_chars(report_file, aws_api):
@@ -122,8 +123,26 @@ def password_reuse_preventrion(report_file, aws_api):
                 rf.write(f"password_reuse_prevention is set correctly as 24\n")
 
 
-# no_root_access_key_exist("report", aws)
-# multifactor_auth_for_root("report", aws)
-# when_root_was_last_used("report", aws)
-# password_longer_than_14_chars("report", aws)
+def mfa_enabled_for_all_users(report_file, aws_api):
+    with open("credential_report.csv", "r") as cr:
+        csv_reader = csv.reader(cr, delimiter=",")
+        for row in csv_reader:
+            if row[3] == "true":
+                if row[7] == "false":
+                    with open(report_file, 'a') as rf:
+                        rf.write(
+                            f"ALERT! User {row[0]} does not have mfa enabled\n")
+                else:
+                    with open(report_file, 'a') as rf:
+                        rf.write(f"User {row[0]} has mfa enabled\n")
+
+
+# def check_for_unused_keys(report_file, aws_api):
+
+generate_and_save_credntial_report("report", aws)
+no_root_access_key_exist("report", aws)
+multifactor_auth_for_root("report", aws)
+when_root_was_last_used("report", aws)
+password_longer_than_14_chars("report", aws)
 password_reuse_preventrion("report", aws)
+mfa_enabled_for_all_users("report", aws)
