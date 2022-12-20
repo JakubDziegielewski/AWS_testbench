a8b6028342870572231e8d131eebd6039679e02e
JakubDziegielewski <kubadziegiel1@gmail.com>
2022-12-20 18:32:08 +0100

    2.3.2

--- lib/rds_test.py
+++ lib/rds_test.py
@@ -17,14 +17,25 @@ def get_all_db_instances(report_file, aws_api, region):
         db_instances= json.loads(output)["DBInstances"]
         return db_instances
 
-@signal_when_test_starts_and_finishes
-def encryption_is_enabled_for_rds_instances(report_file, aws_api, regions):
+def check_one_setting_in_all_rds_instances(report_file, aws_api, regions, setting):
     for region in regions:
         db_instances= get_all_db_instances(report_file, aws_api, region)
         for db_instance in db_instances:
-            if not db_instance["StorageEncrypted"]:
-                write_message_in_report(
-                report_file, f"ALERT: Encryption is not enabled in the database with id: {db_instance['DBInstanceIdentifier']}")
+            if not db_instance[setting]:
+                 write_message_in_report(
+                report_file, f"ALERT: {setting} is not enabled in the database with id: {db_instance['DBInstanceIdentifier']}")
+
+
+@signal_when_test_starts_and_finishes
+def encryption_is_enabled_for_rds_instances(report_file, aws_api, regions):
+    check_one_setting_in_all_rds_instances(report_file, aws_api, regions, "StorageEncrypted")
+
+@signal_when_test_starts_and_finishes
+def auto_minor_version_upgrade_feature_is_enabled_for_rds_instances(report_file, aws_api, regions):
+    check_one_setting_in_all_rds_instances(report_file, aws_api, regions, "AutoMinorVersionUpgrade")
+
+
 
 
 encryption_is_enabled_for_rds_instances("rds_report", aws, ["us-east-1", "eu-central-1"])
+auto_minor_version_upgrade_feature_is_enabled_for_rds_instances("rds_report", aws, ["us-east-1", "eu-central-1"])
