2b4badff544593b269699614be4e3b1d39294a76
JakubDziegielewski <kubadziegiel1@gmail.com>
2022-12-20 15:03:30 +0100

    2.1.3

--- lib/s3_test.py
+++ lib/s3_test.py
@@ -60,23 +60,51 @@ def s3_buckets_employ_encryption_at_rest(report_file, aws_api):
                         report_file, f"ALERT: Server side encryption is not applied by default")
 
 
-
 @signal_when_test_starts_and_finishes
 def s3_bucket_policy_is_set_to_deny_http_requests(report_file, aws_api):
-        buckets = get_list_of_s3_buckets(report_file, aws_api)
-        for bucket in buckets:
-            name = bucket["Name"]
-            policy = get_specific_bucket_configuration(
+    buckets = get_list_of_s3_buckets(report_file, aws_api)
+    for bucket in buckets:
+        name = bucket["Name"]
+        policy = get_specific_bucket_configuration(
             report_file, aws_api, "get-bucket-policy", name, "Policy")
-            if policy != None:
-                statement = json.loads(policy)["Statement"]
-                for statement_property in statement:
-                    if statement_property["Effect"] == "Deny" and statement_property["Condition"]["Bool"]["aws:SecureTransport"] == "false":
-                        write_message_in_report(
-                            report_file, f"Bucket {name} denies http requests {name}")
-                        break
-                else:
+        if policy != None:
+            statement = json.loads(policy)["Statement"]
+            for statement_property in statement:
+                if statement_property["Effect"] == "Deny" and statement_property["Condition"]["Bool"]["aws:SecureTransport"] == "false":
                     write_message_in_report(
-                            report_file, f"ALERT: Bucket {name} does not deny http requests {name}")
+                        report_file, f"Bucket {name} denies http requests {name}")
+                    break
+            else:
+                write_message_in_report(
+                    report_file, f"ALERT: Bucket {name} does not deny http requests {name}")
+
+
+@signal_when_test_starts_and_finishes
+def mfa_delete_is_enabled(report_file, aws_api):
+    buckets = get_list_of_s3_buckets(report_file, aws_api)
+    for bucket in buckets:
+        name = bucket["Name"]
+        bucket_versioning = aws_api.execute(
+            ["s3api", "get-bucket-versioning", "--bucket", name, "--output", "json"])
+        if bucket_versioning:  # bucket_versioning will be an empty string if versioning is not enabled
+            properties = json.loads(bucket_versioning)
+            if "MfaDelete" in properties and properties["MfaDelete"] == "Enabled":
+                write_message_in_report(
+                    report_file, f"MFA Delete is enabled in {name} bucket"
+                )
+
+            else:
+                write_message_in_report(
+                    report_file, f"ALERT: MFA Delete is not enabled in {name} bucket"
+                )
+        else:
+            write_message_in_report(
+                report_file, f"ALERT: Versioning is not enabled in {name} bucket"
+            )
+
+
+"""
 s3_buckets_employ_encryption_at_rest("s3_report", aws)
 s3_bucket_policy_is_set_to_deny_http_requests("s3_report", aws)
+mfa_delete_is_enabled("s3_report", aws)
+"""
