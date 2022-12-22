e1fd4255da3bfbf9584b773231e9fd95845313de
JakubDziegielewski <kubadziegiel1@gmail.com>
2022-12-22 12:59:06 +0100

    3.6 3.7 3.8

--- lib/logging_test.py
+++ lib/logging_test.py
@@ -11,7 +11,6 @@ def describe_trials(report_file):
     return json.loads(output)["trailList"]
 
 
-
 def get_trail_status(report_file, name):
     output = make_request_to_aws(report_file, [
         "cloudtrail", "get-trail-status", "--name", name])
@@ -47,6 +46,34 @@ def check_if_read_write_and_include_management_events_in_event_selectors(event_s
         if "ReadWriteType" in event_selector and "IncludeManagementEvents" in event_selector:
             if event_selector["ReadWriteType"] == "All" and event_selector["IncludeManagementEvents"]:
                 return True
+    return False
+
+
+def get_bucket_acl_grants(report_file, name):
+    output = make_request_to_aws(report_file, [
+                                 "s3api", "get-bucket-acl", "--bucket", name])
+    return json.loads(output)["Grants"]
+
+
+def check_for_group_access_in_acl(grants, uri):
+    for grant in grants:
+        grantee = grant["Grantee"]
+        if grantee["Type"] == "Group" and "URI" in grantee and grantee["URI"] == uri:
+            return True
+    return False
+
+
+def get_bucket_policy(report_file, name):
+    output = make_request_to_aws(
+        report_file, ["s3api", "get-bucket-policy", "--bucket", name])
+    return json.loads(output)["Policy"]
+
+
+def check_if_policy_allows_all_actions(policy):
+    statement = json.loads(policy)["Statement"]
+    for rule in statement:
+        if rule["Effect"] == "Allow" and rule["Principal"] == "*":
+            return True
     return False
 
 
@@ -88,34 +115,6 @@ def cloudtrail_log_file_validation_is_enabled(report_file):
                 report_file, f"ALERT: Cloudtrail {trail['Name']} does not have log file validation enabled")
 
 
-def get_bucket_acl_grants(report_file, name):
-    output = make_request_to_aws(report_file, [
-                                 "s3api", "get-bucket-acl", "--bucket", name])
-    return json.loads(output)["Grants"]
-
-
-def check_for_group_access_in_acl(grants, uri):
-    for grant in grants:
-        grantee = grant["Grantee"]
-        if grantee["Type"] == "Group" and "URI" in grantee and grantee["URI"] == uri:
-            return True
-    return False
-
-
-def get_bucket_policy(report_file, name):
-    output = make_request_to_aws(
-        report_file, ["s3api", "get-bucket-policy", "--bucket", name])
-    return json.loads(output)["Policy"]
-
-
-def check_if_policy_allows_all_actions(policy):
-    statement = json.loads(policy)["Statement"]
-    for rule in statement:
-        if rule["Effect"] == "Allow" and rule["Principal"] == "*":
-            return True
-    return False
-
-
 @signal_when_test_starts_and_finishes
 def s3_bucket_used_to_store_cloudtrail_logs_is_not_publicly_accessible(report_file):
     write_message_in_report(report_file, "Control 3.3")
@@ -153,37 +152,101 @@ def trails_are_integrated_with_cloudwatch_logs(report_file):
         if "CloudWatchLogsLogGroupArn" in trial:
             trail_status = get_trail_status(report_file, name)
             if "LatestCloudWatchLogsDeliveryTime" in trail_status and find_age_of_setting(trail_status["LatestCloudWatchLogsDeliveryTime"]) < 1:
-                write_message_in_report(report_file, f"CloudTrail {name} is integrated with CloudWatch and works properly")
+                write_message_in_report(
+                    report_file, f"CloudTrail {name} is integrated with CloudWatch and works properly")
             else:
-                write_message_in_report(report_file, f"ALERT: CloudTrail {name} is integrated with CloudWatch but does not work properly")
+                write_message_in_report(
+                    report_file, f"ALERT: CloudTrail {name} is integrated with CloudWatch but does not work properly")
         else:
-            write_message_in_report(report_file, f"ALERT: CloudTrail {name} is not integrated with CloudWatch")
-
+            write_message_in_report(
+                report_file, f"ALERT: CloudTrail {name} is not integrated with CloudWatch")
 
 
 @signal_when_test_starts_and_finishes
 def aws_config_is_enabled_in_all_regions(report_file):
     write_message_in_report(report_file, "Control 3.5")
-    configuration_recorders_text = make_request_to_aws(report_file, ["configservice", "describe-configuration-recorders"])
-    configuration_recorders = json.loads(configuration_recorders_text)["ConfigurationRecorders"]
+    configuration_recorders_text = make_request_to_aws(
+        report_file, ["configservice", "describe-configuration-recorders"])
+    configuration_recorders = json.loads(configuration_recorders_text)[
+        "ConfigurationRecorders"]
     for configuration_recorder in configuration_recorders:
         if configuration_recorder["recordingGroup"]["allSupported"] and configuration_recorder["recordingGroup"]["includeGlobalResourceTypes"]:
-            write_message_in_report(report_file, "There exist a configurtion recorder that supports all resource types")
-            configuration_recorder_status_text = make_request_to_aws(report_file, ["configservice", "describe-configuration-recorder-status"])
-            configuration_recorder_status = json.loads(configuration_recorder_status_text)["ConfigurationRecordersStatus"]
+            write_message_in_report(
+                report_file, "There exist a configurtion recorder that supports all resource types")
+            configuration_recorder_status_text = make_request_to_aws(
+                report_file, ["configservice", "describe-configuration-recorder-status"])
+            configuration_recorder_status = json.loads(configuration_recorder_status_text)[
+                "ConfigurationRecordersStatus"]
             for status in configuration_recorder_status:
                 if status["recording"] and status["lastStatus"] == "SUCCESS":
-                    write_message_in_report(report_file, "Configuration recorder works properly")
+                    write_message_in_report(
+                        report_file, "Configuration recorder works properly")
                     break
             else:
-                write_message_in_report(report_file, "ALERT: default configuration recorder does not work properly")
+                write_message_in_report(
+                    report_file, "ALERT: default configuration recorder does not work properly")
             break
     else:
-        write_message_in_report(report_file, "ALERT: AWS Config does not have a recorder that supports all resource types")
+        write_message_in_report(
+            report_file, "ALERT: AWS Config does not have a recorder that supports all resource types")
+
+
+@signal_when_test_starts_and_finishes
+def s3_bucket_access_logging_is_enabled_on_the_cloudtrail_s3_bucket(report_file):
+    write_message_in_report(report_file, "Control 3.6")
+    buckets_for_clodtrails_text = make_request_to_aws(
+        report_file, ["cloudtrail", "describe-trails", "--query", "trailList[*].S3BucketName"])
+    buckets_for_clodtrails = json.loads(buckets_for_clodtrails_text)
+    for bucket in buckets_for_clodtrails:
+        logging_enabled = make_request_to_aws(
+            report_file, ["s3api", "get-bucket-logging", "--bucket", bucket])
+        if logging_enabled:
+            write_message_in_report(
+                report_file, f"Bucket {bucket} has logging enabled")
+        else:
+            write_message_in_report(
+                report_file, f"ALERT: bucket {bucket} does not have logging enabled")
+
+
+@signal_when_test_starts_and_finishes
+def cloudtrail_logs_are_encrypted_at_rest_using_kms_cmk(report_file):
+    write_message_in_report(report_file, "Control 3.7")
+    trails = describe_trials(report_file)
+    for trail in trails:
+        if "KmsKeyId" in trail:
+            write_message_in_report(
+                report_file, f"cloudtrial {trail['Name']} logs are encrypted at rest using KMS CMKs")
+        else:
+            write_message_in_report(
+                report_file, f"ALERT: cloudtrial {trail['Name']} logs are not encrypted at rest using KMS CMKs")
+
+
+
+@signal_when_test_starts_and_finishes
+def rotation_for_customer_created_summetric_cmks_is_enabled(report_file):
+    write_message_in_report(report_file, "Control 3.8")
+    key_list_text = make_request_to_aws(report_file, ["kms", "list-keys"])
+    key_list = json.loads(key_list_text)["Keys"]
+    for key in key_list:
+        key_id = key["KeyId"]
+        key_details_text = make_request_to_aws(report_file, ["kms", "describe-key", "--key-id", key_id])
+        key_details = json.loads(key_details_text)["KeyMetadata"]
+        if "KeySpec" in key_details and key_details["KeySpec"] == "SYMMETRIC_DEFAULT":
+            rotation_status_text = make_request_to_aws(report_file, ["kms", "get-key-rotation-status", "--key-id", key_id])
+            rotation_status = json.loads(rotation_status_text)
+            if "KeyRotationEnabled" in rotation_status and rotation_status["KeyRotationEnabled"]:
+                write_message_in_report(report_file, f"Key with id {key_id} has rotation enabled")
+            else:
+                write_message_in_report(report_file, f"ALERT: Key with id {key_id} does not have rotation enabled")
+
+
 """
 cloudtrail_is_enabled_in_all_regions("logging_report")
 cloudtrail_log_file_validation_is_enabled("logging_report")
 s3_bucket_used_to_store_cloudtrail_logs_is_not_publicly_accessible(
     "logging_report")
 trails_are_integrated_with_cloudwatch_logs("logging_report")
-aws_config_is_enabled_in_all_regions("logging_report")"""
+aws_config_is_enabled_in_all_regions("logging_report")
+s3_bucket_access_logging_is_enabled_on_the_cloudtrail_s3_bucket("logging_report")
+cloudtrail_logs_are_encrypted_at_rest_using_kms_cmk("logging_report")
+rotation_for_customer_created_summetric_cmks_is_enabled("logging_report")"""
