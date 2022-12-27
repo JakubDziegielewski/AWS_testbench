63f2f75f2a84c096e538ec260843721c1517e1e3
JakubDziegielewski <kubadziegiel1@gmail.com>
2022-12-27 17:52:43 +0100

    4.3-4.15

--- lib/monitoring_test.py
+++ lib/monitoring_test.py
@@ -9,8 +9,20 @@ date = datetime.now().strftime("%Y_%m_%d")
 
 patterns = {
     "unauthorized_api_calls": '{ ($.errorCode = *UnauthorizedOperation) || ($.errorCode = AccessDenied*) || ($.sourceIPAddress!=delivery.logs.amazonaws.com) || ($.eventName!=HeadBucket) }',
-    "management_console_sign_in_without_mfa": '{ ($.eventName = "ConsoleLogin") && ($.additionalEventData.MFAUsed != "Yes") }'
-
+    "management_console_sign_in_without_mfa": '{ ($.eventName = "ConsoleLogin") && ($.additionalEventData.MFAUsed != "Yes") }',
+    "usage_of_root_account": '{ $.userIdentity.type = "Root" && $.userIdentity.invokedBy NOT EXISTS && $.eventType != "AwsServiceEvent" }',
+    "iam_policy_changes": '{($.eventName=DeleteGroupPolicy)||($.eventName=DeleteRolePolicy)||($.eventName=DeleteUserPolicy)||($.eventName=PutGroupPolicy)||($.eventName=PutRolePolicy)||($.eventName=PutUserPolicy)||($.eventName=CreatePolicy)||($.eventName=DeletePolicy)||($.eventName=CreatePolicyVersion)||($.eventName=DeletePolicyVersion)||($.eventName=AttachRolePolicy)||($.eventName=DetachRolePolicy)||($.eventName=AttachUserPolicy)||($.eventName=DetachUserPolicy)||($.eventName=AttachGroupPolicy)||($.eventName=DetachGroupPolicy)}',
+    "cloudtrail_configuration_changes": '{ ($.eventName = CreateTrail) || ($.eventName =UpdateTrail) || ($.eventName = DeleteTrail) || ($.eventName = StartLogging)|| ($.eventName = StopLogging) }',
+    "aws_management_console_authentication_failures": '{ ($.eventName = ConsoleLogin) && ($.errorMessage = "Failed authentication") }',
+    "disabling_or_scheduled_deletion_of_customer_created_cmks": '{($.eventSource = kms.amazonaws.com) && (($.eventName=DisableKey)||($.eventName=ScheduleKeyDeletion)) }',
+    "s3_bucket_policy_changes": '{ ($.eventSource = s3.amazonaws.com) && (($.eventName = PutBucketAcl) || ($.eventName = PutBucketPolicy) || ($.eventName = PutBucketCors) || ($.eventName = PutBucketLifecycle) || ($.eventName = PutBucketReplication) || ($.eventName = DeleteBucketPolicy) || ($.eventName = DeleteBucketCors) || ($.eventName = DeleteBucketLifecycle) || ($.eventName = DeleteBucketReplication)) }',
+    "aws_config_configuration_changes": '{ ($.eventSource = config.amazonaws.com) && (($.eventName=StopConfigurationRecorder)||($.eventName=DeleteDeliveryChannel)||($.eventName=PutDeliveryChannel)||($.eventName=PutConfigurationRecorder)) }',
+    "security_group_changes": '{ ($.eventName = AuthorizeSecurityGroupIngress) || ($.eventName = AuthorizeSecurityGroupEgress) || ($.eventName = RevokeSecurityGroupIngress) || ($.eventName = RevokeSecurityGroupEgress) || ($.eventName = CreateSecurityGroup) || ($.eventName = DeleteSecurityGroup) }',
+    "changes_to_network_access_control_lists": '{ ($.eventName = CreateNetworkAcl) || ($.eventName = CreateNetworkAclEntry) || ($.eventName = DeleteNetworkAcl) || ($.eventName = DeleteNetworkAclEntry) || ($.eventName = ReplaceNetworkAclEntry) || ($.eventName = ReplaceNetworkAclAssociation) }',
+    "changes_to_network_gateways": '{ ($.eventName = CreateCustomerGateway) || ($.eventName = DeleteCustomerGateway) || ($.eventName = AttachInternetGateway) || ($.eventName = CreateInternetGateway) || ($.eventName = DeleteInternetGateway) || ($.eventName = DetachInternetGateway) }',
+    "route_table_changes": '{ ($.eventName = CreateRoute) || ($.eventName = CreateRouteTable) || ($.eventName = ReplaceRoute) || ($.eventName = ReplaceRouteTableAssociation) || ($.eventName = DeleteRouteTable) || ($.eventName = DeleteRoute) || ($.eventName = DisassociateRouteTable) }',
+    "vpc_changes": '{ ($.eventName = CreateVpc) || ($.eventName = DeleteVpc) || ($.eventName = ModifyVpcAttribute) || ($.eventName = AcceptVpcPeeringConnection) || ($.eventName = CreateVpcPeeringConnection) || ($.eventName = DeleteVpcPeeringConnection) || ($.eventName = RejectVpcPeeringConnection) || ($.eventName = AttachClassicLinkVpc) || ($.eventName = DetachClassicLinkVpc) || ($.eventName = DisableVpcClassicLink) || ($.eventName = EnableVpcClassicLink) }',
+    "aws_organizations_changes": '{ ($.eventSource = organizations.amazonaws.com) && (($.eventName = "AcceptHandshake") || ($.eventName = "AttachPolicy") || ($.eventName = "CreateAccount") || ($.eventName = "CreateOrganizationalUnit") || ($.eventName = "CreatePolicy") || ($.eventName = "DeclineHandshake") || ($.eventName = "DeleteOrganization") || ($.eventName = "DeleteOrganizationalUnit") || ($.eventName = "DeletePolicy") || ($.eventName = "DetachPolicy") || ($.eventName = "DisablePolicyType") || ($.eventName = "EnablePolicyType") || ($.eventName = "InviteAccountToOrganization") || ($.eventName = "LeaveOrganization") || ($.eventName = "MoveAccount") || ($.eventName = "RemoveAccountFromOrganization") || ($.eventName = "UpdatePolicy") || ($.eventName = "UpdateOrganizationalUnit")) }'
 }
 
 
@@ -175,7 +187,104 @@ def log_metric_filter_and_alarm_exist_for_management_console_sign_in_without_mfa
         report_file, "management_console_sign_in_without_mfa")
 
 
-# describe_trials("monitoring_report")
+@signal_when_test_starts_and_finishes
+def log_metric_filter_and_alarm_exist_for_usage_of_root_account(report_file):
+    write_message_in_report(report_file, "Control 4.3")
+    log_metric_filter_and_alarm_exist_for_setting(
+        report_file, "usage_of_root_account")
+
+
+@signal_when_test_starts_and_finishes
+def log_metric_filter_and_alarm_exist_for_iam_policy_changes(report_file):
+    write_message_in_report(report_file, "Control 4.4")
+    log_metric_filter_and_alarm_exist_for_setting(
+        report_file, "iam_policy_changes")
+
+
+@signal_when_test_starts_and_finishes
+def log_metric_filter_and_alarm_exist_for_cloudtrail_configuration_changes(report_file):
+    write_message_in_report(report_file, "Control 4.5")
+    log_metric_filter_and_alarm_exist_for_setting(
+        report_file, "cloudtrail_configuration_changes")
+
+
+@signal_when_test_starts_and_finishes
+def log_metric_filter_and_alarm_exist_for_aws_management_console_authentication_failures(report_file):
+    write_message_in_report(report_file, "Control 4.6")
+    log_metric_filter_and_alarm_exist_for_setting(
+        report_file, "aws_management_console_authentication_failures")
+
+
+@signal_when_test_starts_and_finishes
+def log_metric_filter_and_alarm_exist_for_disabling_or_scheduled_deletion_of_customer_created_cmks(report_file):
+    write_message_in_report(report_file, "Control 4.7")
+    log_metric_filter_and_alarm_exist_for_setting(
+        report_file, "disabling_or_scheduled_deletion_of_customer_created_cmks")
+
+
+@signal_when_test_starts_and_finishes
+def log_metric_filter_and_alarm_exist_for_s3_bucket_policy_changes(report_file):
+    write_message_in_report(report_file, "Control 4.8")
+    log_metric_filter_and_alarm_exist_for_setting(
+        report_file, "s3_bucket_policy_changes")
+
+
+@signal_when_test_starts_and_finishes
+def log_metric_filter_and_alarm_exist_for_aws_config_configuration_changes(report_file):
+    write_message_in_report(report_file, "Control 4.9")
+    log_metric_filter_and_alarm_exist_for_setting(
+        report_file, "aws_config_configuration_changes")
+
+
+@signal_when_test_starts_and_finishes
+def log_metric_filter_and_alarm_exist_for_security_group_changes(report_file):
+    write_message_in_report(report_file, "Control 4.10")
+    log_metric_filter_and_alarm_exist_for_setting(
+        report_file, "security_group_changes")
+
+
+@signal_when_test_starts_and_finishes
+def log_metric_filter_and_alarm_exist_for_changes_to_network_access_control_lists(report_file):
+    write_message_in_report(report_file, "Control 4.11")
+    log_metric_filter_and_alarm_exist_for_setting(
+        report_file, "changes_to_network_access_control_lists")
+
+
+@signal_when_test_starts_and_finishes
+def log_metric_filter_and_alarm_exist_for_changes_to_network_gateways(report_file):
+    write_message_in_report(report_file, "Control 4.12")
+    log_metric_filter_and_alarm_exist_for_setting(
+        report_file, "changes_to_network_gateways")
+
+
+@signal_when_test_starts_and_finishes
+def log_metric_filter_and_alarm_exist_for_route_table_changes(report_file):
+    write_message_in_report(report_file, "Control 4.13")
+    log_metric_filter_and_alarm_exist_for_setting(
+        report_file, "route_table_changes")
+
+
+@signal_when_test_starts_and_finishes
+def log_metric_filter_and_alarm_exist_for_vpc_changes(report_file):
+    write_message_in_report(report_file, "Control 4.14")
+    log_metric_filter_and_alarm_exist_for_setting(
+        report_file, "vpc_changes")
+
+
+@signal_when_test_starts_and_finishes
+def log_metric_filter_and_alarm_exist_for_aws_organizations_changes(report_file):
+    write_message_in_report(report_file, "Control 4.15")
+    log_metric_filter_and_alarm_exist_for_setting(
+        report_file, "aws_organizations_changes")
+
+
+"""
+describe_trials("monitoring_report")
 log_metric_filter_and_alarm_exist_for_unauthorized_API_calls(
     "monitoring_report")
-log_metric_filter_and_alarm_exist_for_management_console_sign_in_without_mfa("monitoring_report")
+log_metric_filter_and_alarm_exist_for_management_console_sign_in_without_mfa(
+    "monitoring_report")
+log_metric_filter_and_alarm_exist_for_iam_policy_changes("monitoring_report")
+
+log_metric_filter_and_alarm_exist_for_route_table_changes("monitoring_report")
+"""
